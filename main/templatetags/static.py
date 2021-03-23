0ea28a2b47d085ab32380f879f79a212ef278765
cool-hammer <ajtwlstmdgks@gmail.com>
2021-03-23 17:10:55 +0900

    templatetags/static.py modified

--- main/templatetags/static.py
+++ main/templatetags/static.py
@@ -3,12 +3,12 @@ from django.templatetags.static import StaticNode, register
 
 register = template.Library()
 
-from datetime import date
+from datetime import datetime
 
 class CustomStaticNode(StaticNode):
     
     def url(self, context):
-        version = date.today()
+        version = datetime.now().strftime('%Y-%m-%d-%H')
         path = f'{super().url(context)}?v={version}'
         return path
     
